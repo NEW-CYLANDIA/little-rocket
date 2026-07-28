e38ce3bc7e73133a63f16bffb439773596469476
Taylor Anderson <taylor-anderson@hotmail.ca>
2026-07-28 17:42:59 -0400

    Implementing separate collision shape for bullets

--- components/gun/bullet/bullet.gd
+++ components/gun/bullet/bullet.gd
@@ -4,10 +4,11 @@ class_name Bullet
 var owned_by_ship:bool = false;
 @export var speed:int = 50;
 var velocity:Vector2 = Vector2.ZERO;
+@onready var wall_detector = $WallDetector
 func _ready():
 	top_level = true;
-	area_entered.connect(func(_a): queue_free());
-	body_entered.connect(func(_b): queue_free())
+	wall_detector.area_entered.connect(func(_a): queue_free());
+	wall_detector.body_entered.connect(func(_b): queue_free())
 func _physics_process(delta):
 	position += velocity * delta;
 	sprite.rotation = velocity.angle();
