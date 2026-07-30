45974dc07535e4b7cdb4c10dfeafb7cd1bc29aea
Taylor Anderson <taylor-anderson@hotmail.ca>
2026-07-30 19:17:12 -0400

    small fixes

--- systems/camera_2d.gd
+++ systems/camera_2d.gd
@@ -3,5 +3,5 @@ extends Camera2D
 @export var target:Ship;
 
 func _process(_delta):
-	if target.alive:
+	if is_instance_valid(target):
 		position = position.lerp(target.position + target.linear_velocity * 0.8, 0.02);
