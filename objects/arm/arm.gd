81aa0c6ec6ab1df46a97fdb9f34fd2377fbf4602
Taylor Anderson <taylor-anderson@hotmail.ca>
2026-07-30 21:15:05 -0400

    fixing the armz

--- objects/arm/arm.gd
+++ objects/arm/arm.gd
@@ -5,9 +5,11 @@ extends Node2D
 ## The extend position is set to wherever the attachment is currently.
 @export var retract_position_reference:Node2D
 
-@export var speed:float = 10.0;
+@export var speed:float = 150.0;
 
 @export var use_physics:bool = false;
+# used in physics mode only
+@export var damping_force:float = 10.0;
 
 var retract_position:Vector2;
 var extend_position:Vector2;
@@ -47,13 +49,12 @@ func _process(delta):
 		if attachment is SimpleBody and use_physics:
 			attachment.gravity_scale = 0;
 			var force = (current_dest - attachment.position)
-			print(force)
-			attachment.add_constant_force(force)       
+			attachment.apply_central_force(force * speed - attachment.linear_velocity * damping_force)       
 		else:
 			if attachment.position.distance_to(current_dest) < 1:
 				attachment.position = current_dest;
 			else:
-				attachment.position = attachment.position.lerp(current_dest, delta * speed)
+				attachment.position = attachment.position.lerp(current_dest, delta * delta * speed)
 	
 func extend(distance:float = 1):
 	retracted = false;
